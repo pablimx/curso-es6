1bd9b08302e9e4e20ff433891171b741fc31489a
Pablo Resende <pablim@live.co.uk>
2020-07-07 14:26:29 -0300

    Classes

--- main.js
+++ main.js
@@ -1,5 +1,29 @@
-class Teste {
-  metodo() {}
-
-  outro() {}
+class List {
+  constructor() {
+    this.data = [];
+  }
+  add(data) {
+    this.data.push(data);
+    console.log(this.data);
+  }
 }
+
+class TodoList extends List {
+  constructor() {
+    super();
+
+    this.usuario = "Usuário Teste";
+  }
+
+  mostraUsuario() {
+    console.log(this.usuario);
+  }
+}
+
+var MinhaLista = new TodoList();
+
+document.getElementById("novotodo").onclick = function () {
+  MinhaLista.add("Novo todo");
+};
+
+MinhaLista.mostraUsuario();
