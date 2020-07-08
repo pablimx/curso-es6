63afadd24a3b141d76793d3f2a0af5f09216d0ec
Pablo Resende <pablim@live.co.uk>
2020-07-08 12:20:44 -0300

    Template Literals

--- bundle.js
+++ bundle.js
@@ -1,59 +1,7 @@
 "use strict";
 
-function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+//TEMPLATE LITERALS
+var name = "Pablo";
+var age = "31"; //console.log("Meu nome é " + name + " e tenho " + age + " anos");
 
-function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(Object(source), true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
-
-function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
-
-function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
-
-function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
-
-//REST
-var user = {
-  name: "Osvair",
-  age: "50",
-  company: "Cia & Teste"
-};
-
-var name = user.name,
-    others = _objectWithoutProperties(user, ["name"]);
-
-console.log(name);
-console.log(others);
-var array = [1, 2, 3, 4];
-var a = array[0],
-    b = array[1],
-    c = array.slice(2);
-console.log(a);
-console.log(b);
-console.log(c);
-
-function sum() {
-  for (var _len = arguments.length, params = new Array(_len), _key = 0; _key < _len; _key++) {
-    params[_key] = arguments[_key];
-  }
-
-  return params.reduce(function (total, next) {
-    return total + next;
-  });
-}
-
-console.log(sum(1, 2, 3, 4)); //SPREAD
-
-var array1 = [1, 2, 3];
-var array2 = [4, 5, 6];
-var array3 = [].concat(array1, array2);
-console.log(array3);
-var user1 = {
-  name: "Osvair",
-  age: "50",
-  company: "Cia & Teste"
-};
-
-var user2 = _objectSpread(_objectSpread({}, user1), {}, {
-  name: "Osvaldo"
-});
-
-console.log(user2);
+console.log("Meu nome \xE9 ".concat(name, " e tenho ").concat(age, " anos."));
--- main.js
+++ main.js
@@ -1,44 +1,5 @@
-//REST
-
-const user = {
-  name: "Osvair",
-  age: "50",
-  company: "Cia & Teste",
-};
-
-const { name, ...others } = user;
-
-console.log(name);
-console.log(others);
-
-const array = [1, 2, 3, 4];
-
-const [a, b, ...c] = array;
-
-console.log(a);
-console.log(b);
-console.log(c);
-
-function sum(...params) {
-  return params.reduce((total, next) => total + next);
-}
-
-console.log(sum(1, 2, 3, 4));
-
-//SPREAD
-
-const array1 = [1, 2, 3];
-const array2 = [4, 5, 6];
-const array3 = [...array1, ...array2];
-
-console.log(array3);
-
-const user1 = {
-  name: "Osvair",
-  age: "50",
-  company: "Cia & Teste",
-};
-
-const user2 = { ...user1, name: "Osvaldo" };
-
-console.log(user2);
+//TEMPLATE LITERALS
+const name = "Pablo";
+const age = "31";
+//console.log("Meu nome é " + name + " e tenho " + age + " anos");
+console.log(`Meu nome é ${name} e tenho ${age} anos.`);
