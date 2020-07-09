77ffc79b927471dbb8b47fde63898a9be0f51dc8
Pablo Resende <pablim@live.co.uk>
2020-07-09 20:25:06 -0300

    Exporting and importing functions

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-import { soma } from "./funcoes";
+import * as funcoes from "./funcoes";
+// import soma, { sub } from "./funcoes";
+// import { soma as somaFunction, sub } from "./funcoes";
+// import somaFunction from "./soma";
 
-console.log(soma(4, 3));
+console.log(funcoes);
+
+// console.log(sub(4, 3));
+// console.log(soma(4, 3));
+
+console.log(funcoes.sub(5, 6));
+console.log(funcoes.soma(4, 6));
