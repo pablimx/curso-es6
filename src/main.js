8dcfdcd3e8f5e8a07fcaebc4062a43a2c2e2b2b7
Pablo Resende <pablim@live.co.uk>
2020-07-09 21:25:28 -0300

    Async and Await

--- src/main.js
+++ src/main.js
@@ -1,11 +1,41 @@
-import ClasseUsuario from "./functions.js";
+const minhaPromise = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("OK");
+    }, 2000);
+  });
 
-ClasseUsuario.info();
+// minhaPromise()
+//   .then((response) => {
+//     console.log(response);
+//   })
+//   .catch((error) => {});
 
-import { idade } from "./functions";
+// async function executaPromise() {
+// const response = await minhaPromise();
 
-console.log(idade);
+// console.log(response);
+// console.log(await minhaPromise());
+// console.log(await minhaPromise());
+// console.log(await minhaPromise());
 
-import Usuario, { idade as IdadeUsuario } from "./functions";
+// minhaPromise().then((response) => {
+//   console.log(response);
 
-console.log(IdadeUsuario);
+//   minhaPromise().then((response) => {
+//     console.log(response);
+
+//     minhaPromise().then((response) => {
+//       console.log(response);
+//     });
+//   });
+// });
+// }
+
+const executaPromise = async () => {
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+  console.log(await minhaPromise());
+};
+
+executaPromise();
