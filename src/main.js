7aa79da387a85ae014d96a60d4ba58b926bcc9b9
Pablo Resende <pablim@live.co.uk>
2020-07-09 21:00:56 -0300

    Exercises

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-alert("Teste");
+import ClasseUsuario from "./functions.js";
+
+ClasseUsuario.info();
+
+import { idade } from "./functions";
+
+console.log(idade);
+
+import Usuario, { idade as IdadeUsuario } from "./functions";
+
+console.log(IdadeUsuario);
