1596c552f5daaf565d8b4770ef8d37f8ee1a3053
Pablo Resende <pablim@live.co.uk>
2020-07-20 13:22:08 -0300

    Aplicação ES6+

--- src/main.js
+++ src/main.js
@@ -1,17 +1,62 @@
-import axios from "axios";
+class App {
+  constructor() {
+    this.repositories = [];
 
-class Api {
-  static async getUserInfo(username) {
-    try {
-      const response = await axios.get(
-        `https://api.github.com/users/${username}`
-      );
-      console.log(response);
-    } catch {
-      console.warn("Erro na API");
-    }
+    this.formElement = document.getElementById("repo-form");
+    this.listEl = document.getElementById("repo-list");
+
+    this.registerHandlers();
+  }
+
+  registerHandlers() {
+    this.formElement.onsubmit = (event) => this.addRepository(event);
+  }
+
+  addRepository(event) {
+    event.preventDefault();
+
+    this.repositories.push({
+      name: "rocketseat.com.br",
+      description: "Tire a sua idéia do papel e dê vida à sua startup.",
+      avatar_url: "https://avatars0.githubusercontent.com/u/28929274?v=4",
+      html_url: "http://github.com/rocketseat/unform",
+    });
+    this.render();
+  }
+
+  render() {
+    this.listEl.innerHTML = "";
+
+    this.repositories.forEach((repo) => {
+      let imgEl = document.createElement("img");
+      imgEl.setAttribute("src", repo.avatar_url);
+
+      let titleEl = document.createElement("strong");
+      titleEl.appendChild(document.createTextNode(repo.name));
+
+      let descriptionEl = document.createElement("p");
+      descriptionEl.appendChild(document.createTextNode(repo.description));
+
+      let linkEl = document.createElement("a");
+      linkEl.setAttribute("target", "_blank");
+      linkEl.appendChild(document.createTextNode("Acessar"));
+
+      let listItemEl = document.createElement("li");
+      listItemEl.appendChild(imgEl);
+      listItemEl.appendChild(titleEl);
+      listItemEl.appendChild(descriptionEl);
+      listItemEl.appendChild(linkEl);
+
+      this.listEl.appendChild(listItemEl);
+
+      // <img src="https://avatars0.githubusercontent.com/u/28929274?v=4" />
+      //   <strong>rocketseat.com.br</strong>
+      //   <p>Tire a sua idéia do papel e dê vida à sua startup.</p>
+      //   <a target="_blank" href="http://github.com/rocketseat/unform"
+      //     >Acessar</a
+      //   >
+    });
   }
 }
 
-Api.getUserInfo("pablimx");
-Api.getUserInfo("pablimx123456");
+new App();
