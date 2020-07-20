01c7c68f3e4d21b367de9ee6fd4e7e61b965d838
Pablo Resende <pablim@live.co.uk>
2020-07-20 14:01:25 -0300

    Recebendo dados da API

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
+import api from "./api";
+
 class App {
   constructor() {
     this.repositories = [];
 
     this.formElement = document.getElementById("repo-form");
+    this.inputEl = document.querySelector("input[name=repository]");
     this.listEl = document.getElementById("repo-list");
 
     this.registerHandlers();
@@ -12,15 +15,31 @@ class App {
     this.formElement.onsubmit = (event) => this.addRepository(event);
   }
 
-  addRepository(event) {
+  async addRepository(event) {
     event.preventDefault();
 
+    const repoInput = this.inputEl.value;
+
+    if (repoInput.length === 0) return;
+
+    const response = await api.get(`/repos/${repoInput}`);
+
+    const {
+      name,
+      description,
+      html_url,
+      owner: { avatar_url },
+    } = response.data;
+
     this.repositories.push({
-      name: "rocketseat.com.br",
-      description: "Tire a sua idéia do papel e dê vida à sua startup.",
-      avatar_url: "https://avatars0.githubusercontent.com/u/28929274?v=4",
-      html_url: "http://github.com/rocketseat/unform",
+      name,
+      description,
+      avatar_url,
+      html_url,
     });
+
+    this.inputEl.value = "";
+
     this.render();
   }
 
@@ -39,6 +58,7 @@ class App {
 
       let linkEl = document.createElement("a");
       linkEl.setAttribute("target", "_blank");
+      linkEl.setAttribute("href", repo.html_url);
       linkEl.appendChild(document.createTextNode("Acessar"));
 
       let listItemEl = document.createElement("li");
